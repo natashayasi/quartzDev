313fbe3de03293a399bad35dcf34789bb8b9e9ad
Natasha Yasi <natashayasi@gmail.com>
2024-05-07 20:06:28 -0400

    Tidy, Add White/Blacklist, Disable option

--- quartz/components/Properties.tsx
+++ quartz/components/Properties.tsx
@@ -2,7 +2,9 @@ import { FullSlug, TransformOptions, transformLink} from "../util/path"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import  style from "./styles/properties.scss"
 
-var propertiesExcluded = ["draft","tags","title"] // Add properties you want to ignore here
+var disablePropertiesComponent = false // enable/disable here
+var propertiesExcluded: string [] = ["draft","tags","title"] // Blacklisted properties
+var propertiesIncluded: string [] = [] // Whitelisted properties
 
 function createLinkedElement(fileData: any, opts : any, value: string) {
   let cleanedValue = value.replace(/['"\[\]]+/g, '')
@@ -14,6 +16,7 @@ function createLinkedElement(fileData: any, opts : any, value: string) {
 }
 
 function createPropertyElement(key: string, value: any) {
+
   return(
     <li>
       <span class="property">{key}</span> : <span class="value">{value}</span>
@@ -22,16 +25,18 @@ function createPropertyElement(key: string, value: any) {
 }
 
 function stringifyValues(strs: any) {
-    var valueArray: string[] = [];
-    if (strs && typeof strs === "object") {
-      for (const s of strs) {
-        valueArray.push(s);
-      }
-    } 
-    else if (typeof strs === "string") {
-        valueArray.push(strs);
+  var valueArray: string[] = [];
+
+  if (strs && typeof strs === "object") {
+    for (const s of strs) {
+      valueArray.push(s);
     }
-    return valueArray;
+  } 
+  else if (typeof strs === "string") {
+      valueArray.push(strs);
+  }
+
+  return valueArray;
 }
 
 export default (() => {
@@ -41,39 +46,52 @@ export default (() => {
       allSlugs: allFiles.map((fp) => fp.slug as FullSlug)
     }
 
+    if(disablePropertiesComponent){
+      return null
+    }
+
     var propertiesElements = []
 
     if(Object.keys(fileData.frontmatter ?? {}).length > 0){
       for(const [key, value] of Object.entries(fileData.frontmatter ?? {})) {
-        if(!propertiesExcluded.includes(key)){
+        
+        var keyIsExcluded = propertiesExcluded.includes(key)
+        var keyIsIncluded = propertiesIncluded.includes(key)
+        
+        // Choose whitelist or blacklist filtering of properties
+        if(!keyIsExcluded){
           var linkedElements = []
           var valueStringArray = stringifyValues(value);
 
           if (valueStringArray.length > 0){
             for(let i=0; i < valueStringArray.length; i++){
-                if(valueStringArray[i].includes("[[")){
-                    if(i > 0){
-                        linkedElements.push(", ")
-                    }
-                    linkedElements.push(createLinkedElement(fileData, opts, valueStringArray[i]))
+              if(valueStringArray[i].includes("[[")){
+                if(i > 0){
+                  linkedElements.push(", ")
                 }
-                else{
-                    linkedElements.push(valueStringArray[i])
-                }
+                linkedElements.push(createLinkedElement(fileData, opts, valueStringArray[i]))
+              }
+              else{
+                linkedElements.push(valueStringArray[i])
+              }
             }
           }
-        propertiesElements.push(createPropertyElement(key, linkedElements))
-      }
 
+          propertiesElements.push(createPropertyElement(key, linkedElements))
+        }
       }
     }
-
-    return (      
-      <div class="properties">
-        <ul>{propertiesElements}</ul>
-      </div>
-    )
-
+    
+    if(propertiesElements.length === 0){
+      return null
+    }
+    else{
+      return (      
+        <div class="properties">
+          <ul>{propertiesElements}</ul>
+        </div>
+      )
+    }
   }
 
     PropertiesWithWorkingLinks.css = style
